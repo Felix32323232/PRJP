9df7e9d075b03563dbb661bce63706c5afcf0e44
Felix32323232 <felixramirochulvi2@gmail.com>
2026-03-11 19:55:04 +0100

    Hacerlo con la E

--- Particulas 3D/Assets/Scripts/ParticleController.cs
+++ Particulas 3D/Assets/Scripts/ParticleController.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class ParticleController : MonoBehaviour
@@ -9,28 +11,38 @@ public class ParticleController : MonoBehaviour
     [SerializeField] private float Gtime;
     [SerializeField] private float Ggravedad;
     [SerializeField] public GameObject Particle;
-    [SerializeField] private float spawnInterval = 0.01f; 
-    private float spawnTimer = 0f;
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+       if (context.started)
+        {
+            CreateParticle();
+        }
+
+       
+        
+    }
+
 
     void Update()
     {
-        spawnTimer += Time.deltaTime;
-        if (spawnTimer >= spawnInterval)
-        {
-            CreateParticle();
-            spawnTimer = 0f;
-            
-        }
+        
     }
 
     void CreateParticle()
     {
-        GameObject par = Instantiate(Particle, transform.position, Quaternion.identity);
-        Particle parScript = par.GetComponent<Particle>();
-        parScript.velIn = GparSpeed * Random.Range(1, 3);
-        parScript.angIn = Gangle * Random.Range(20, 60);
-        parScript.angInZ = Random.Range(0, 361);
-        parScript.tiempoMax = Gtime;
-        parScript.gravedad = Ggravedad * Random.Range(1, 3);
+        for (int i = 0; i < numPar; i++)
+        {
+            GameObject par = Instantiate(Particle, transform.position, Quaternion.identity);
+            Particle parScript = par.GetComponent<Particle>();
+            parScript.velIn = GparSpeed * Random.Range(1, 3);
+            parScript.angIn = Gangle * Random.Range(20, 60);
+            parScript.angInZ = Random.Range(0, 361);
+            parScript.tiempoMax = Gtime;
+            parScript.gravedad = Ggravedad * Random.Range(1, 3);
+        }
+           
     }
+
+ 
 }
